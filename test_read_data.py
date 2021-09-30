0d9d323594a67b20352ceb835ba3d6f3361735d8
Nicolas Bichon <nbichon@gmail.com>
2021-09-30 14:20:26 +0200

    final test version

---
+++ test_read_data.py
@@ -0,0 +1,19 @@
+import read_data
+
+
+def test_fistname():
+    assert read_data.ispresent(r'adresse.csv','Bob')
+
+def test_name():
+    assert read_data.ispresent(r'adresse.csv','Bisson')
+
+
+
+def test_fistname():
+    assert read_data.ispresent(r'adresse.csv','Bob')
+
+def test_name():
+    assert read_data.ispresent(r'adresse.csv','Bisson')
+
+
+
